c10171510d05ea8be2599dee87d11ff619bd8c51
moeyad7 <mohamed.eyad2612@gmail.com>
2023-05-05 17:45:18 +0300

    added logout

--- lib/compnents/app_bar.dart
+++ lib/compnents/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 AppBar my_appBar(BuildContext context) {
   return AppBar(
@@ -11,5 +12,32 @@ AppBar my_appBar(BuildContext context) {
       ),
     ),
     foregroundColor: ThemeData().colorScheme.primary,
+    actions: [
+      DropdownButton(
+        icon: Icon(
+          Icons.more_vert,
+          color: Theme.of(context).primaryIconTheme.color,
+        ),
+        items: [
+          DropdownMenuItem(
+            child: Container(
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.exit_to_app),
+                  SizedBox(width: 8),
+                  Text('Logout'),
+                ],
+              ),
+            ),
+            value: 'logout',
+          )
+        ],
+        onChanged: (itemIdentifier) {
+          if (itemIdentifier == 'logout') {
+            FirebaseAuth.instance.signOut();
+          }
+        },
+      )
+    ],
   );
 }
