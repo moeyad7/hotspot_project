9a1c44732ed021214ccedd7e3f986f36603f600f
moeyad7 <mohamed.eyad2612@gmail.com>
2023-05-10 22:50:39 +0300

    fixed

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -20,78 +20,72 @@ class _CreatePostState extends State<CreatePost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MyAppBar(context),
-        body: SingleChildScrollView(child: Column(
+      appBar: MyAppBar(context),
+      body: SingleChildScrollView(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
               padding: EdgeInsets.all(30),
               child: Text('New Post',
-               style: TextStyle(
+                  style: TextStyle(
                     fontSize: 30,
-                    fontWeight: FontWeight.bold, 
-                    color: ThemeData().colorScheme.primary, 
-                  )            
-            
-              ),
+                    fontWeight: FontWeight.bold,
+                    color: ThemeData().colorScheme.primary,
+                  )),
             ),
-          
             Container(
-          
               width: MediaQuery.of(context).size.width * 0.8,
               padding: EdgeInsets.all(15),
               child: TextField(
-                  decoration: InputDecoration(
-                          labelText: "Title",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          filled: true,
-                          fillColor: Color(0xFFF58A07).withOpacity(0.3),
-                        ),
+                decoration: InputDecoration(
+                  labelText: "Title",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFFF58A07).withOpacity(0.3),
+                ),
                 style: TextStyle(
                   fontSize: 24,
-                  fontWeight: FontWeight.bold,  
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
             Container(
-                padding: EdgeInsets.all(30),
-                height:MediaQuery.of(context).size.width * 0.6 ,
+              padding: EdgeInsets.all(30),
+              height: MediaQuery.of(context).size.width * 0.6,
               child: TextField(
-                // Single-line fields automatically scroll to the right when the text field is full  
-                maxLines: 10,
-                decoration: InputDecoration(
-                          labelText: "Description",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          filled: true,
-                          fillColor: Color(0xFFF58A07).withOpacity(0.3),
-                        ),
-                style: TextStyle(
-                  fontSize: 18,
-                )
-              ),
+                  // Single-line fields automatically scroll to the right when the text field is full
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    labelText: "Description",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFFF58A07).withOpacity(0.3),
+                  ),
+                  style: TextStyle(
+                    fontSize: 18,
+                  )),
             ),
             Container(
               alignment: Alignment.topLeft,
               padding: EdgeInsets.only(left: 10),
               child: Text('Categories',
-               style: TextStyle(
+                  style: TextStyle(
                     fontSize: 30,
-                    fontWeight: FontWeight.bold, 
-                    color: ThemeData().colorScheme.primary, 
-                  )            
-            
-              ),
+                    fontWeight: FontWeight.bold,
+                    color: ThemeData().colorScheme.primary,
+                  )),
             ),
-
-        ],),   
+          ],
         ),
-          bottomNavigationBar: NavBarComponent(
-        selectedTab: NavigationItem.home,
       ),
-        );
+      bottomNavigationBar: NavBarComponent(
+        selectedTab: NavigationItem.add,
+      ),
+    );
   }
 }
