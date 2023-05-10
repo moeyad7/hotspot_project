3f912568d9fd0bf70440a44eae61c52be48b1826
moeyad7 <mohamed.eyad2612@gmail.com>
2023-05-10 23:13:07 +0300

    added change password button

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -40,6 +40,8 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   var _dateOfBirth = TextEditingController();
   var _userImage = TextEditingController();
 
+  var _password = '';
+
   @override
   void initState() {
     // TODO: implement initState
@@ -126,9 +128,78 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                     },
                   ),
                   SizedBox(height: 10),
-                  Row(
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
+                      ElevatedButton.icon(
+                        icon: const Icon(Icons.edit),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xFF228CE5)),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                        label: const Text('Change Password'),
+                        onPressed: () {
+                          //change the password
+                          showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text('Change Password'),
+                              content: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: TextField(
+                                      autofocus: true,
+                                      decoration: const InputDecoration(
+                                          labelText: 'New Password',
+                                          hintText: 'Enter your new password'),
+                                      onChanged: (value) {
+                                        _password = value;
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.pop(context, 'Cancel'),
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context, 'OK'),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            ),
+                          ).then((returnVal) {
+                            if (returnVal == 'OK') {
+                              //change the password
+                              setState(
+                                () {},
+                              );
+                            }
+                          });
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        height: 40,
+                        width: 122,
+                        child: CustomButton(
+                          name: "Edit Image",
+                          color: Color(0xFFD6D6D6),
+                          pressFunction: () {},
+                        ),
+                      ),
+                      SizedBox(height: 10),
                       Container(
                         height: 40,
                         width: 150,
@@ -138,15 +209,6 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                           pressFunction: () {},
                         ),
                       ),
-                      Container(
-                        height: 40,
-                        width: 122,
-                        child: CustomButton(
-                          name: "Edit Image",
-                          color: Color(0xFFD6D6D6),
-                          pressFunction: () {},
-                        ),
-                      )
                     ],
                   )
                 ],
