32f177ecfdb2b9808895d6e4250065bd0ca13495
mosama0100 <83070845+mosama0100@users.noreply.github.com>
2023-05-17 17:54:38 +0300

    addedd stuff to login required

--- lib/screens/account_required.dart
+++ lib/screens/account_required.dart
@@ -5,6 +5,8 @@ import '../compnents/nav_bar.dart';
 import '../screens/auth_screen.dart';
 import '../compnents/buttons/buttons.dart';
 
+import 'package:lottie/lottie.dart';
+
 class AccountRequiredScreen extends StatelessWidget {
   const AccountRequiredScreen({Key? key}) : super(key: key);
   static const routeName = '/account-required';
@@ -13,32 +15,33 @@ class AccountRequiredScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(context),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Lottie.asset(
-            //   'assets/animations/Login Required Animation.json',
-            //   width: MediaQuery.of(context).size.width * 0.8,
-            //   height: MediaQuery.of(context).size.width * 0.8,
-            // ),
-            SizedBox(height: 20),
-            Text(
-              'You must have an account to access this page.',
-              style: TextStyle(fontSize: 20),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            Container(
-                width: MediaQuery.of(context).size.width * 0.6,
-                child: CustomButton(
-                  name: "Create Account",
-                  color: Color(0xFFF58A07),
-                  pressFunction: () {
-                    Navigator.of(context).pushNamed(AuthScreen.routeName);
-                  },
-                ))
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Lottie.network(
+              //     'https://assets10.lottiefiles.com/packages/lf20_8xjzqz.json',
+              //     width: 300,
+              //     height: 300),
+              // SizedBox(height: 20),
+              Text(
+                'You must have an account to access this page.',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: CustomButton(
+                    name: "Create Account",
+                    color: Color(0xFFF58A07),
+                    pressFunction: () {
+                      Navigator.of(context).pushNamed(AuthScreen.routeName);
+                    },
+                  ))
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: NavBarComponent(selectedTab: NavigationItem.home),
