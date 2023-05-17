092d093d06c312063d524095f251cc66db0b87fe
AlHassanHK <al.hassan.elkady@gmail.com>
2023-05-17 15:34:37 +0300

    savescreen future builder error

--- lib/screens/saved_screen.dart
+++ lib/screens/saved_screen.dart
@@ -4,45 +4,98 @@ import 'package:flutter/material.dart';
 
 import '../compnents/app_bar.dart';
 import '../compnents/nav_bar.dart';
+import '../compnents/cards/post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class SavedScreen extends StatelessWidget {
+import '../model/tourist_site.dart';
+
+class SavedScreen extends StatefulWidget {
   static const routeName = '/SavedScreen';
 
+  @override
+  State<SavedScreen> createState() => _SavedScreenState();
+}
+
+class _SavedScreenState extends State<SavedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(context),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              height: 45,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .doc(FirebaseAuth.instance.currentUser!.uid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (!snapshot.hasData || snapshot.data == null) {
+              // Stream has no data, show a message
+              return Center(
+                child: Text('No data available.'),
+              );
+            }
+            final userDocs = snapshot.data!;
+            print(userDocs["saved"]);
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Icon(
-                    Icons.bookmark,
-                    color: Color(0xFFF58A07),
+                  Container(
+                    height: 45,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.bookmark,
+                          color: Color(0xFFF58A07),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Saved • " + userDocs["saved"].length.toString(),
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(
-                    width: 10,
+                  Divider(
+                    color: Colors.black,
+                    thickness: 2,
                   ),
-                  Text(
-                    "Saved • " + "69",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
+                  //  userDocs["saved"].map<PostCard>((postId) async {
+                  //   final locationDocs = await FirebaseFirestore.instance
+                  //       .collection("locations")
+                  //       .doc(postId)
+                  //       .get();
+                  //       print(locationDocs["title"]);
+                  //   return FutureBuilder (
+                  //     future: locationDocs ,
+                  //     builder:((BuildContext context, snapshot) {
+                  //       PostCard(
+                  //       touristSites: TouristSite(
+                  //         id: locationDocs.id,
+                  //         title: locationDocs['title'],
+                  //         description: locationDocs['description'],
+                  //         imageUrl: locationDocs['image'],
+                  //         category: List<String>.from(locationDocs['categories']),
+                  //         added: locationDocs['time'].toDate(),
+                  //       ),
+                  //     );
+                  //     } ) 
+                  //   );
+                  // }).toList(),
                 ],
               ),
-            ),
-            Divider(
-              color: Colors.black,
-              thickness: 2,
-            ),
-          ],
-        ),
-      ),
+            );
+          }),
       bottomNavigationBar: NavBarComponent(selectedTab: NavigationItem.profile),
     );
   }
