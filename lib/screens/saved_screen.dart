00e7cf9ea25d571c6ff8ee9644a63bd0050f8228
moeyad7 <mohamed.eyad2612@gmail.com>
2023-05-17 15:53:22 +0300

    finished saved and visited screens

--- lib/screens/saved_screen.dart
+++ lib/screens/saved_screen.dart
@@ -41,58 +41,77 @@ class _SavedScreenState extends State<SavedScreen> {
               );
             }
             final userDocs = snapshot.data!;
-            print(userDocs["saved"]);
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 45,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.bookmark,
-                          color: Color(0xFFF58A07),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "Saved • " + userDocs["saved"].length.toString(),
-                          style: TextStyle(
-                              color: Colors.black, fontWeight: FontWeight.bold),
-                        ),
-                      ],
+            return SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 45,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.bookmark,
+                            color: Color(0xFFF58A07),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Saved • " + userDocs["saved"].length.toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Divider(
-                    color: Colors.black,
-                    thickness: 2,
-                  ),
-                  //  userDocs["saved"].map<PostCard>((postId) async {
-                  //   final locationDocs = await FirebaseFirestore.instance
-                  //       .collection("locations")
-                  //       .doc(postId)
-                  //       .get();
-                  //       print(locationDocs["title"]);
-                  //   return FutureBuilder (
-                  //     future: locationDocs ,
-                  //     builder:((BuildContext context, snapshot) {
-                  //       PostCard(
-                  //       touristSites: TouristSite(
-                  //         id: locationDocs.id,
-                  //         title: locationDocs['title'],
-                  //         description: locationDocs['description'],
-                  //         imageUrl: locationDocs['image'],
-                  //         category: List<String>.from(locationDocs['categories']),
-                  //         added: locationDocs['time'].toDate(),
-                  //       ),
-                  //     );
-                  //     } ) 
-                  //   );
-                  // }).toList(),
-                ],
+                    Divider(
+                      color: Colors.black,
+                      thickness: 2,
+                    ),
+                    StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('locations')
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+
+                          if (!snapshot.hasData || snapshot.data == null) {
+                            // Stream has no data, show a message
+                            return Center(
+                              child: Text('No data available.'),
+                            );
+                          }
+                          return ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: userDocs["saved"].length,
+                            itemBuilder: (context, index) {
+                              return PostCard(
+                                touristSites: TouristSite(
+                                  id: userDocs["saved"][index],
+                                  title: snapshot.data!.docs[index]['title'],
+                                  description: snapshot.data!.docs[index]
+                                      ['description'],
+                                  imageUrl: snapshot.data!.docs[index]['image'],
+                                  category: List<String>.from(
+                                      snapshot.data!.docs[index]['categories']),
+                                  added: snapshot.data!.docs[index]['time']
+                                      .toDate(),
+                                ),
+                              );
+                            },
+                          );
+                        }),
+                  ],
+                ),
               ),
             );
           }),
--- lib/screens/visited_screen.dart
+++ lib/screens/visited_screen.dart
@@ -4,45 +4,117 @@ import 'package:flutter/material.dart';
 
 import '../compnents/app_bar.dart';
 import '../compnents/nav_bar.dart';
+import '../compnents/cards/post.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class VisitedScreen extends StatelessWidget {
+import '../model/tourist_site.dart';
+
+class VisitedScreen extends StatefulWidget {
   static const routeName = '/VisitedScreen';
 
+  @override
+  State<VisitedScreen> createState() => _VisitedScreenState();
+}
+
+class _VisitedScreenState extends State<VisitedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(context),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              height: 45,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.check_circle,
-                    color: Color(0xFF2FC686),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "Visited • " + "69",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                ],
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("users")
+              .doc(FirebaseAuth.instance.currentUser!.uid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (!snapshot.hasData || snapshot.data == null) {
+              // Stream has no data, show a message
+              return Center(
+                child: Text('No data available.'),
+              );
+            }
+            final userDocs = snapshot.data!;
+            return SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 45,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.check_circle,
+                            color: Color(0xFF2FC686),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "Visited • " + userDocs["seen"].length.toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      thickness: 2,
+                    ),
+                    StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('locations')
+                            .snapshots(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+
+                          if (!snapshot.hasData || snapshot.data == null) {
+                            // Stream has no data, show a message
+                            return Center(
+                              child: Text('No data available.'),
+                            );
+                          }
+                          return ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: userDocs["seen"].length,
+                            itemBuilder: (context, index) {
+                              return PostCard(
+                                touristSites: TouristSite(
+                                  id: userDocs["seen"][index],
+                                  title: snapshot.data!.docs[index]['title'],
+                                  description: snapshot.data!.docs[index]
+                                      ['description'],
+                                  imageUrl: snapshot.data!.docs[index]['image'],
+                                  category: List<String>.from(
+                                      snapshot.data!.docs[index]['categories']),
+                                  added: snapshot.data!.docs[index]['time']
+                                      .toDate(),
+                                ),
+                              );
+                            },
+                          );
+                        }),
+                  ],
+                ),
               ),
-            ),
-            Divider(
-              color: Colors.black,
-              thickness: 2,
-            ),
-          ],
-        ),
-      ),
+            );
+          }),
       bottomNavigationBar: NavBarComponent(selectedTab: NavigationItem.profile),
     );
   }
