3c6e5991f00d4995aa2594cb901c11a69bc46318
moeyad7 <mohamed.eyad2612@gmail.com>
2023-05-22 13:48:31 +0300

    filter by category in home screen doen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,7 +21,53 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     final fbm = FirebaseMessaging.instance;
     fbm.requestPermission();
-    
+  }
+
+  List<String> _inactiveChips = [
+    'Nature',
+    'History',
+    'Entertainment',
+    'Food',
+    'Adventure',
+    'Religion',
+    'Shopping',
+    'Culture'
+  ];
+  List<String> _activeChips = [];
+
+  Widget _buildChips() {
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 8.0,
+      children: _inactiveChips
+          .map((chip) => ChoiceChip(
+                label: Text(
+                  chip,
+                  style: TextStyle(
+                    color: _activeChips.contains(chip)
+                        ? Colors.white
+                        : Colors.black,
+                  ),
+                ),
+                selected: _activeChips.contains(chip),
+                onSelected: (selected) => _activateChip(selected, chip),
+                selectedColor: Colors.blue,
+                backgroundColor: _activeChips.contains(chip)
+                    ? Color(0xFF228CE5).withOpacity(0.4)
+                    : Color(0xFF228CE5).withOpacity(0.4),
+              ))
+          .toList(),
+    );
+  }
+
+  void _activateChip(bool selected, String chip) {
+    setState(() {
+      if (selected) {
+        _activeChips.add(chip);
+      } else {
+        _activeChips.remove(chip);
+      }
+    });
   }
 
   @override
@@ -31,6 +77,18 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Container(
+              padding: EdgeInsets.only(left: 30, top: 10, right: 30),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(children: [
+                  Wrap(
+                      spacing: 3.0,
+                      // gap between adjacent chips
+                      children: [_buildChips()]),
+                ]),
+              ),
+            ),
             StreamBuilder(
               stream: FirebaseFirestore.instance
                   .collection('locations')
@@ -51,23 +109,51 @@ class _HomePageState extends State<HomePage> {
 
                 final locationDocs = snapshot.data!.docs;
 
+                if (_activeChips.isEmpty) {
+                  return ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: locationDocs.length,
+                    itemBuilder: (context, index) {
+                      return PostCard(
+                        touristSites: TouristSite(
+                          id: locationDocs[index].id,
+                          title: locationDocs[index]['title'],
+                          description: locationDocs[index]['description'],
+                          imageUrl: locationDocs[index]['image'],
+                          category: List<String>.from(
+                              locationDocs[index]['categories']),
+                          added: locationDocs[index]['time'].toDate(),
+                          ratings: locationDocs[index]['ratings'],
+                        ),
+                      );
+                    },
+                  );
+                }
+
                 return ListView.builder(
                   physics: NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
                   itemCount: locationDocs.length,
                   itemBuilder: (context, index) {
-                    return PostCard(
-                      touristSites: TouristSite(
-                        id: locationDocs[index].id,
-                        title: locationDocs[index]['title'],
-                        description: locationDocs[index]['description'],
-                        imageUrl: locationDocs[index]['image'],
-                        category: List<String>.from(
-                            locationDocs[index]['categories']),
-                        added: locationDocs[index]['time'].toDate(),
-                        ratings: locationDocs[index]['ratings'],
-                      ),
-                    );
+                    for (var i = 0; i < _activeChips.length; i++) {
+                      if (locationDocs[index]['categories']
+                          .contains(_activeChips[i])) {
+                        return PostCard(
+                          touristSites: TouristSite(
+                            id: locationDocs[index].id,
+                            title: locationDocs[index]['title'],
+                            description: locationDocs[index]['description'],
+                            imageUrl: locationDocs[index]['image'],
+                            category: List<String>.from(
+                                locationDocs[index]['categories']),
+                            added: locationDocs[index]['time'].toDate(),
+                            ratings: locationDocs[index]['ratings'],
+                          ),
+                        );
+                      }
+                    }
+                    return Container();
                   },
                 );
               },
