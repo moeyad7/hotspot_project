93268312b4904e3eb403f40e30765409c457c348
3mkTawfik <iamsoepic20022@gmail.com>
2023-05-29 00:04:46 +0300

    added the chat to the correct area

--- lib/compnents/comments/message_bubble.dart
+++ lib/compnents/comments/message_bubble.dart
@@ -4,14 +4,21 @@ class MessageBubble extends StatelessWidget {
   MessageBubble(this.message, this.isMe);
   final String message;
   final bool isMe;
+
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: <Widget>[
         Container(
           decoration: BoxDecoration(
-            color: Color(0xFF228CE5),
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Color(0xFF228CE5) : Colors.grey[300],
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+            ),
           ),
           width: 140,
           padding: EdgeInsets.symmetric(
@@ -24,7 +31,9 @@ class MessageBubble extends StatelessWidget {
           ),
           child: Text(
             message,
-            style: TextStyle(color: Colors.white),
+            style: TextStyle(
+              color: isMe ? Colors.white : Colors.black,
+            ),
           ),
         ),
       ],
